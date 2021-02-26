b745b96f58f3c9665cc08180cb8ad204dee22769
Alex <Alex@DESKTOP-T4COF2N>
2021-02-25 20:05:08 -0600

    commit de Alexander

---
+++ src/minombre57297/MiNombre57297.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minombre57297;
+
+/**
+ *
+ * @author Alex
+ */
+public class MiNombre57297 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Alexander de Jesus Medina Colli");
+    }
+    
+}
